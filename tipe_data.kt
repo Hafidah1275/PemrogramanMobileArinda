60dff7d08a473e7c0a16b667710ddaa383d0444e
Hafidah1275 <102033015+Hafidah1275@users.noreply.github.com>
2022-09-12 17:14:46 +0700

    Add files via upload

---
+++ tipe_data.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var nama:String="arinda"
+    var alamat:String="""
+        Saya tinggal di BENGKALIS
+        Belajar di Politeknik Negeri Bengkalis
+        di Prodi Rekayasa Perangkat Lunak
+    """.trimIndent()
+
+    println(nama)
+    println("Tentang "+alamat)
+}
